147a37ce11fbec95dedd46f3322b9eafce1520cc
Olegas Polonskis <olegas.polonskis@stud.vilniustech.lt>
2022-12-17 16:21:15 +0200

    Commit one

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -1,20 +1,247 @@
-// Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+bool exitGame;
+bool gameOver;
+bool isMoving;
+bool bombExplode;
+bool dead;
+int height;
+int width;
+int life = 5;
+int speed;
+int score;
+int myCarX;
+int myCarY;
+int highScore;
+int highestScore[5];
+int enemyX[4];
+int enemyY[4] = { -8, -18, -28, -38 };
+int enemyPositionX;
+int enemyPositionY;
+int enemyNum;
+
+string bombParticle1[4] = { "o   o"," ooo "," ooo " ,"o   o" };
+
+string bombParticle2[4] = { " ooo " ,"o   o","o   o"," ooo " };
+
+string myCar[4] = { " # ","# #", " # ", "# #" };
+
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void hideCursor() {
+    CONSOLE_CURSOR_INFO cursor;
+    cursor.dwSize = 100;
+    cursor.bVisible = false;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor);
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void startUp() {
+    srand(time(NULL));
+    exitGame = false;
+    isMoving = false;
+    gameOver = false;
+    dead = false;
+    height = 20;
+    width = 19;
+    myCarX = 8;
+    myCarY = 16;
+    speed = 1;
+    enemyNum = 4;
+    score = 0;
+    for (int i = 0; i < enemyNum; i++) {
+        enemyPositionX = rand() % 3;
+        if (enemyPositionX == 0) enemyX[i] = 2;
+        else if (enemyPositionX == 1) enemyX[i] = 8;
+        else if (enemyPositionX == 2) enemyX[i] = 14;
+    }
+    enemyY[0] = -8;
+    enemyY[1] = -18;
+    enemyY[2] = -28;
+    enemyY[3] = -38;
+
+}
+
+void layout() {
+    for (int i = 0; i < height; i++) {
+        gotoxy(0, i); cout << "#               #";
+        if (i % 2 == 0 && isMoving) {
+            gotoxy(6, i); cout << "|    |";
+        }
+        else if (i % 2 != 0 && !isMoving) {
+            gotoxy(6, i); cout << "|    |";
+        }
+    }
+    gotoxy(5, 21); cout << "Life : " << life;
+    gotoxy(5, 22); cout << "Score: " << score;
+}
+void drawMyCar() {
+    if (!dead) {
+        for (int i = 0; i < 4; i++) {
+            gotoxy(myCarX, myCarY + i); cout << myCar[i];
+        }
+    }
+    else {
+        for (int i = 0; i < 4; i++) {
+            gotoxy(myCarX, myCarY + i); cout << "   ";
+        }
+    }
+}
+
+void drawEnemyCar() { // to print enemy car
+    for (int i = 0; i < enemyNum; i++) {
+        if (enemyY[i] + 3 > 0) {
+            gotoxy(enemyX[i], enemyY[i] + 3);
+            cout << " # ";
+        }if (enemyY[i] + 2 > 6) {
+            gotoxy(enemyX[i], enemyY[i] + 2);
+            cout << "# #";
+        }if (enemyY[i] + 1 > 0) {
+            gotoxy(enemyX[i], enemyY[i] + 1);
+            cout << " # ";
+        }if (enemyY[i] > 0) {
+            gotoxy(enemyX[i], enemyY[i]);
+            cout << "# #";
+        }if (enemyY[i] + 3 >= 20) {
+            gotoxy(enemyX[i], enemyY[i] + 3);
+            cout << "   ";
+        }if (enemyY[i] + 2 >= 20) {
+            gotoxy(enemyX[i], enemyY[i] + 2);
+            cout << "   ";
+        }if (enemyY[i] + 1 >= 20) {
+            gotoxy(enemyX[i], enemyY[i] + 1);
+            cout << "   ";
+        }if (enemyY[i] >= 20) {
+            gotoxy(enemyX[i], enemyY[i]);
+            cout << "   ";
+        }
+    }
+}
+
+void input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+        case 72:
+            if (myCarY > 0 && !dead) myCarY = myCarY - speed;
+            break;
+        case 75:
+            if (myCarX > 2 && !dead) myCarX = myCarX - 6;
+            break;
+        case 77:
+            if (myCarX < 14 && !dead) myCarX = myCarX + 6;
+            break;
+        case 80:
+            if (myCarY < 16 && !dead) myCarY = myCarY + speed;
+            break;
+        }
+    }
+}
+
+void rules_logic() {
+    srand(time(NULL));
+
+    if (isMoving) isMoving = false;
+    else isMoving = true;
+
+    for (int i = 0; i < enemyNum; i++) {
+        enemyY[i]++;
+    }
+
+    for (int i = 0; i < enemyNum; i++) {
+        if (enemyY[i] > 21) {
+            enemyPositionX = rand() % 3;
+            if (enemyPositionX == 0) enemyX[i] = 2;
+            else if (enemyPositionX == 1) enemyX[i] = 8;
+            else if (enemyPositionX == 2) enemyX[i] = 14;
+            enemyY[i] = -8;
+            score++;
+        }
+    }
+    for (int i = 0; i < enemyNum; i++) {
+        if ((myCarY <= enemyY[i] + 3 && myCarY >= enemyY[i] || enemyY[i] >= myCarY && enemyY[i] <= myCarY + 3) && myCarX == enemyX[i]) {
+            dead = true;
+        }
+    }
+}
+
+//no bomb explosion
+
+void died() {
+    if (dead) {
+        life--;
+        int count = 0;
+        while (count != 10) {
+            input();
+            //bomb explosion call
+            gotoxy(2, 22); cout << "You got " << score << " Score!";
+            count++;
+
+        }
+        gotoxy(2, 22); cout << "                 ";
+        highestScore[life] = score;
+        startUp();
+    }
+}
+
+void transition() {
+    for (int i = 19; i >= 0; i--) {
+        gotoxy(1, i); cout << "#################";
+        Sleep(15);
+    }
+    for (int i = 1; i < 20; i++) {
+        gotoxy(1, i); cout << "                 ";
+        Sleep(15);
+    }
+}
+
+void game_Over() {
+    for (int i = 0; i < life; i++) {
+        if (highestScore[i] >= highestScore[i - 1]) highScore = highestScore[i];
+        else if (highestScore[i] <= highestScore[i - 1]) highScore = highestScore[i - 1];
+    }
+
+    if (life == 0) {
+        gameOver = true;
+        do {
+            gotoxy(0, 1); cout << "#################";
+            gotoxy(0, 2); cout << "#               #";
+            gotoxy(0, 3); cout << "#               #";
+            gotoxy(0, 4); cout << "#               #";
+            gotoxy(0, 5); cout << "#  GAME OVER!!  #";
+            gotoxy(0, 6); cout << "#               #";
+            gotoxy(0, 7); cout << "#   HIGHSCORE   #";
+            gotoxy(0, 8); cout << "#               #";
+            gotoxy(0, 9); cout << "#               #";
+            gotoxy(0, 10); cout << "#       " << highScore << "        #";
+            gotoxy(0, 11); cout << "#               #";
+            gotoxy(0, 12); cout << "#               #";
+            gotoxy(0, 13); cout << "#               #";
+            gotoxy(0, 15); cout << "#               #";
+            gotoxy(0, 15); cout << "#   Press  'X'  #";
+            gotoxy(0, 16); cout << "#    TO EXIT    #";
+            gotoxy(0, 17); cout << "#               #";
+            gotoxy(0, 18); cout << "#               #";
+            gotoxy(0, 19); cout << "#################";
+            gotoxy(0, 20); cout << "                 ";
+            gotoxy(0, 21); cout << "                 ";
+        } while (getch() != 'x');
+        exit(1);
+    }
+}
+
+
+
+
+
+
+
